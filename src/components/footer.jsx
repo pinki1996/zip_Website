e026adad01f6b2179bd35d4bc0d233f55a29c582
pinki1996 <120261749+pinki1996@users.noreply.github.com>
2026-04-08 14:03:26 +0530

    whatApp

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
+import React from "react";
 
 export const Footer = () => {
+  const whatsappMessage = `I want to buy this product: `;
+  const whatsappLink = `https://wa.me/919289492133?text=${encodeURIComponent(whatsappMessage)}`;
   return (
     <div id="footer">
-        <div className="container text-center">
-          <p>
-            &copy; 2023 Premium Concealed Zippers by Ziphosa. Design by{" "}
-            {/* <a href="http://www.templatewire.com" rel="nofollow"> */}
-              A Brand of D.A ENTERPRISES
-            {/* </a> */}
-          </p>
-        </div>
+      <a
+        id="whatsapp-link"
+        href={whatsappLink}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="btn btn-md custom-btn"
+      >
+        <i className="fa fa-whatsapp"></i>
+      </a>
+      <div className="container text-center">
+        <p>
+          &copy; 2023 Premium Concealed Zippers by Ziphosa. Design by{" "}
+          {/* <a href="http://www.templatewire.com" rel="nofollow"> */}A Brand
+          of D.A ENTERPRISES
+          {/* </a> */}
+        </p>
       </div>
-  )
-}
+    </div>
+  );
+};
